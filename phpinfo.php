4708f1a48324a1a4afe3c0df953d40740de278e1
gpunty <peter.h.nguyen@me.com>
2022-05-10 22:20:18 +0930

    Current progress
    - Initial version of weeks calculation
    - Initial version of weekday count calculation
    - Fixes to consider ordering of date parameters to ensure correct processing of values

--- phpinfo.php
+++ phpinfo.php
@@ -1,7 +1,7 @@
 <?php
 
 class DateOperator {
-	public static function convertDateToUnit($date) {
+	private static function convertDateToUnit($date) {
 		//Calculate number of days up to but including the current year. The number of days
 		//elapsed in the current year will be calculated via the Z date format
 		$yearVal = $date->format("Y") - 1;
@@ -31,23 +31,61 @@ class DateOperator {
 		$numDaysDate1 = self::convertDateToUnit($date1);
 		$numDaysDate2 = self::convertDateToUnit($date2);
 		
-		return ($numDaysDate1 - $numDaysDate2);
+		return abs($numDaysDate1 - $numDaysDate2);
 	} 
 	
 	public static function calcNumWeekdays($date1, $date2) {
-		return 0;
+	    //Determine the larger of the date values to ensure correct processing
+		$minDate = $date1 >= $date2 ? clone $date2 : clone $date1;
+		$maxDate = $date1 >= $date2 ? clone $date1 : clone $date2;
+		 
+		//Get the day number for the smaller date. This will be used to determine
+		//how many weekdays there are remaining for the first week
+		$dayOfMinDate = $minDate->format("w");
+		$remainingDaysFirstWeekDate1 = 0;
+		$tempDate = clone $minDate;
+		
+		//Day number value of 0 corresponds to a Sunday, which is the start of the week.
+		//If the Day number isn't 0, we can determine that for the first week that there
+		//are 6 - day number days until the next Sunday (6 because we ignore the Sunday;
+		//it signifies the start of a new week)
+		if($dayOfMinDate != 0) {
+		    //6 - day number to get the date of the first Sunday. This also gives us
+		    //the remaining number of weekdays for the first week
+			$remainingDaysFirstWeekDate1 = 6 - $dayOfMinDate;
+			$tempDate->modify("+". ($remainingDaysFirstWeekDate1 + 1). " days");
+		}
+		
+		//Total number of week days is calculated as:
+		//Remaining number of days of first week + (number of complete weeks * 5 days) + day number of date2
+		//NOTE: if the day number date2 is on a Saturday, this means that 5 weekdays have passed for date2 anyway
+		
+		$numWeekdays = $remainingDaysFirstWeekDate1;
+		
+		$numCompleteWeeks = self::calcNumCompleteWeeks($tempDate, $maxDate);
+		$numWeekdays += ($numCompleteWeeks * 5);
+		
+		$elapsedDaysDate2 = $maxDate->format("w");
+		
+		if($elapsedDaysDate2 == 6) {
+			$elapsedDaysDate2--;
+		}   
+		
+		$numWeekdays += $elapsedDaysDate2;
+		
+		return $numWeekdays;
 	}
 
 	public static function calcNumCompleteWeeks($date1, $date2) {
-		//call calcNumDays
-		//divide by 7, take floor value
-		return 0;
+		$numDays = self::calcNumDays($date1, $date2);
+		return floor($numDays / 7);
 	}
 }
 
-$dateVal1 = date_create("2022-03-01");
+$dateVal1 = date_create("2022-03-19");
 $dateVal2 = date_create("2022-02-03");
-echo DateOperator::calcNumdays($dateVal1, $dateVal2), " days";
-//echo DateOperator::convertDateToUnit($dateVal1);
+echo DateOperator::calcNumdays($dateVal1, $dateVal2), " days<br>";
+echo DateOperator::calcNumCompleteWeeks($dateVal1, $dateVal2), " complete weeks<br>";
+echo DateOperator::calcNumWeekdays($dateVal1, $dateVal2), " weekdays<br>";
 ?>
 
